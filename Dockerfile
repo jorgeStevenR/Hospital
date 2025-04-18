bdf932c5b063247d4fd5fc5bb097ac9d1602ca33
jorge rodriguez <rodrigesj500@gmail.com>
2025-04-18 14:18:28 -0500

    Primer commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+
+FROM openjdk:17-jdk-slim
+ARG JAR_FILE=target/hospital-0.0.1.jar
+
+
+CMD ["/bin/sh"]
---
+++ src/test/java/com/example/Hospital/HospitalApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.example.Hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
